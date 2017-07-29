1ef66927026311050e4fdb8d96701d99224615af
jalle walle <dada_kong@live.se>
2017-07-30 00:15:49 +0200

    label funkar

--- försök till bra spel/Ny värld.cs
+++ försök till bra spel/Ny värld.cs
@@ -29,6 +29,7 @@ namespace försök_till_bra_spel
             storlek = 1;
             skapa();
             uncheck();
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -50,37 +51,57 @@ namespace försök_till_bra_spel
         }
         private void skapa()
         {
-            skapavärld.getStorlek(storlek);
+            
+           
             label2.Text = "Hämtar storlek";
+            label2.Update();
+            skapavärld.getStorlek(storlek);
 
-            skapavärld.skapavärlden();
+
             label2.Text = "Skapar terräng";
+            updatelabel();
+            skapavärld.skapavärlden();
 
+
+            label2.Text = "Namnger";
+            updatelabel();
             name = textBox1.Text;
             skapavärld.getvärldnamn(name);
-            label2.Text = "Namnger";
 
-            skapavärld.underworld();
+
             label2.Text = "Lägger ut jord och sten";
+            updatelabel();
+            skapavärld.underworld();
 
-            skapavärld.caves();
+
             label2.Text = "Skapar grottor";
+            updatelabel();
+            skapavärld.caves();
 
-            skapavärld.floatingislands();
+
             label2.Text = "Skapar öar";
+            updatelabel();
+            skapavärld.floatingislands();
 
-            skapavärld.mineraler();
+
             label2.Text = "Skapar mineraler";
+            updatelabel();
+            skapavärld.mineraler();
 
-            skapavärld.skapavatten();
             label2.Text = "Skapar vatten";
+            updatelabel();
+            skapavärld.skapavatten();
+            
 
-            skapavärld.sparavärlden();
             label2.Text = "Sparar";
-
-
-
+            updatelabel();
+            skapavärld.sparavärlden();
+            
             MessageBox.Show("Sparat");
+        }
+        private void updatelabel()
+        {
+            label2.Update();
         }
         public int setstorlek(int x)
         {
